92642d191722413da197a4a63a367d1d8e2cf08c
David Domínguez <dedc_011596@hotmail.com>
2023-12-22 02:44:06 -0600

    Se agrega la url global para los storages

--- feria/urls.py
+++ feria/urls.py
@@ -1,8 +1,11 @@
 # Django
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('apps.users.urls', namespace='users')),
     path('p/', include('apps.places.urls', namespace='places')),
-    path('api/', include('apps.dates.urls'))
-]
+    path('api/', include('apps.dates.urls')),
+    path('__debug__/', include('debug_toolbar.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
