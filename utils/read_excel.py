a36a6b05e1449f3d9df3c5802d841984bac6495c
DEDC <dedc_011596@hotmail.com>
2024-02-21 17:00:03 -0600

    Se agrega una función para leer el excel de ambulantes e insertarlos directo a la bd junto con la cita correspondiente

---
+++ utils/read_excel.py
@@ -0,0 +1,26 @@
+# openpyxl
+from openpyxl import load_workbook
+# users
+from apps.users.models import Usuarios
+# dates
+from apps.dates.models import CitasAgendadas
+
+def read_ambulantes():
+    wb = load_workbook('static/docs/ambulantes.xlsx')
+    ws = wb.active
+    counter = 0
+    
+    for row in ws.iter_rows(min_row=2):
+        counter += 1
+        first_name = str(row[0].value).strip()
+        last_name = str(row[1].value).strip()
+        phone_number = str(row[2].value).strip()
+        email = str(row[3].value).strip().lower()
+        password = str(row[4].value).strip()
+        date = str(row[5].value).strip()
+        hour = str(row[6].value).strip()
+        print(counter, first_name)
+        usu = Usuarios.objects.create(email=email, first_name=first_name, last_name=last_name, phone_number=phone_number)
+        usu.set_password(password)
+        usu.save()
+        date = CitasAgendadas.objects.get_or_create(fecha=date, hora=hour, usuario=usu)
