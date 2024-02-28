c90cc86d6942ad6effce62644d4cd6c1238d05b1
DEDC <dedc_011596@hotmail.com>
2024-02-28 10:44:00 -0600

    Se agregan campos al word de contrato fisica

--- utils/word_writer.py
+++ utils/word_writer.py
@@ -2,12 +2,15 @@
 import io
 import re
 from decimal import Decimal
+from datetime import date
 # python-openxl
 from docx import Document
 # Django
 from django.http import HttpResponse
 from django.db.models import Sum
 from django.contrib.humanize.templatetags.humanize import intcomma
+from django.utils.timezone import localtime
+from django.utils import formats
 # num2words
 from num2words import num2words
 
@@ -17,6 +20,8 @@ def generate_physical_document(request_):
     buffer = io.BytesIO()
     price_places = request_.solicitud_lugar.aggregate(price=Sum('precio'))['price']
     price_iva = round(price_places * Decimal(0.16))
+    month = formats.date_format(date.today(), 'F')
+    day = formats.date_format(date.today(), 'j')
     data = {
         '<razon_social>': request_.nombre,
         '<address>': '{} {} {}'.format(request_.calle, request_.no_calle, request_.codigo_postal),
@@ -29,27 +34,28 @@ def generate_physical_document(request_):
         '<price_iva>': intcomma(price_iva),
         '<price_iva_text>': num2words(price_iva, lang='es').upper(),
         '<price_places_iva>': intcomma(price_places + price_iva),
-        '<price_places_iva_text>': num2words(price_places + price_iva, lang='es').upper()
+        '<price_places_iva_text>': num2words(price_places + price_iva, lang='es').upper(),
+        '<day>': day,
+        '<month>': month,
+        '<name_user>': request_.usuario.get_full_name()
     }
     for p in document.paragraphs:
         if '<table_places>' in p.text:
-            table = document.add_table(rows=1, cols=6, style="Table Grid")
+            table = document.add_table(rows=1, cols=5, style="Table Grid")
             heading_row = table.rows[0].cells
             # add headings
             heading_row[0].text = "No."
             heading_row[1].text = "Zona"
             heading_row[2].text = "m2"
-            heading_row[3].text = "Giro"
-            heading_row[4].text = "Licencia de Alcohol"
-            heading_row[5].text = "Terraza"
+            heading_row[3].text = "Licencia de Alcohol"
+            heading_row[4].text = "Terraza"
             for place in request_.solicitud_lugar.all():
                 row = table.add_row().cells
                 row[0].text = place.nombre
                 row[1].text = place.get_zona_display()
                 row[2].text = str(place.m2)
-                row[3].text = "No definido"
-                row[4].text = "No"
-                row[5].text = "No"
+                row[3].text = "Sí" if place.extras.filter(tipo='alcohol').exists() else 'No'
+                row[4].text = "Sí" if place.extras.filter(tipo='terrazas').exists() else 'No'
             p._p.addnext(table._tbl)
             p.text = ''
         for k, v in data.items():
