8099e2cc6f29d5decedfd913c0647d079e5fd366
DEDC <dedc_011596@hotmail.com>
2024-03-07 19:14:41 -0600

    Se agrega una nueva hoja en el excel del reporte

--- utils/reports.py
+++ utils/reports.py
@@ -1,10 +1,10 @@
 # Django
 from django.http import HttpResponse
+from django.db.models import Q, Sum, Count
 # places
-from apps.places.models import Lugares
+from apps.places.models import Lugares, Pagos, ProductosExtras
 # openpyxl
 from openpyxl import load_workbook
-from openpyxl.drawing.image import Image
 
 def get_report():
     wb = load_workbook('static/docs/layout_feria.xlsx')
@@ -37,6 +37,59 @@ def get_report():
             ws.cell(row=counter, column=counter_column).value = 'Aplica para {}'.format(px.to_places)
             counter_column += 1
         counter += 1
+    ws = wb.get_sheet_by_name('MONTOS')
+    # n1
+    ws['E4'] = Lugares.objects.filter(zona='n_1').count()
+    ws['E8'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar', filter=Q(solicitud__solicitud_lugar__zona='n_1')))['total']
+    ws['H4'] = Lugares.objects.filter(zona='n_1').aggregate(total=Sum('precio'))['total']
+    ws['H8'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__precio', filter=Q(solicitud__solicitud_lugar__zona='n_1')))['total']
+    # n3
+    ws['O4'] = Lugares.objects.filter(zona='n_3').count()
+    ws['O8'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar', filter=Q(solicitud__solicitud_lugar__zona='n_3')))['total']
+    ws['R4'] = Lugares.objects.filter(zona='n_3').aggregate(total=Sum('precio'))['total']
+    ws['R8'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__precio', filter=Q(solicitud__solicitud_lugar__zona='n_3')))['total']
+    # z_a
+    ws['E13'] = Lugares.objects.filter(zona='z_a').count()
+    ws['E17'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar', filter=Q(solicitud__solicitud_lugar__zona='z_a')))['total']
+    ws['H13'] = Lugares.objects.filter(zona='z_a').aggregate(total=Sum('precio'))['total']
+    ws['H17'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__precio', filter=Q(solicitud__solicitud_lugar__zona='z_a')))['total']
+    # z_b
+    ws['O13'] = Lugares.objects.filter(zona='z_b').count()
+    ws['O17'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar', filter=Q(solicitud__solicitud_lugar__zona='z_b')))['total']
+    ws['R13'] = Lugares.objects.filter(zona='z_b').aggregate(total=Sum('precio'))['total']
+    ws['R17'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__precio', filter=Q(solicitud__solicitud_lugar__zona='z_b')))['total']
+    # z_c
+    ws['E22'] = Lugares.objects.filter(zona='z_c').count()
+    ws['E26'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar', filter=Q(solicitud__solicitud_lugar__zona='z_c')))['total']
+    ws['H22'] = Lugares.objects.filter(zona='z_c').aggregate(total=Sum('precio'))['total']
+    ws['H26'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__precio', filter=Q(solicitud__solicitud_lugar__zona='z_c')))['total']
+    # z_d
+    ws['O22'] = Lugares.objects.filter(zona='z_d').count()
+    ws['O26'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar', filter=Q(solicitud__solicitud_lugar__zona='z_d')))['total']
+    ws['R22'] = Lugares.objects.filter(zona='z_d').aggregate(total=Sum('precio'))['total']
+    ws['R26'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__precio', filter=Q(solicitud__solicitud_lugar__zona='z_d')))['total']
+    # terraza
+    ws['E37'] = ProductosExtras.objects.filter(tipo='terraza').count()
+    ws['E41'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar__extras', filter=Q(solicitud__solicitud_lugar__extras__tipo='terraza')))['total']
+    ws['H37'] = ProductosExtras.objects.filter(tipo='terraza').aggregate(total=Sum('precio'))['total']
+    ws['H41'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__extras__precio', filter=Q(solicitud__solicitud_lugar__extras__tipo='terraza')))['total']
+    # terraza_grande
+    ws['O37'] = ProductosExtras.objects.filter(tipo='terraza_grande').count()
+    ws['O41'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar__extras', filter=Q(solicitud__solicitud_lugar__extras__tipo='terraza_grande')))['total']
+    ws['R37'] = ProductosExtras.objects.filter(tipo='terraza_grande').aggregate(total=Sum('precio'))['total']
+    ws['R41'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__extras__precio', filter=Q(solicitud__solicitud_lugar__extras__tipo='terraza_grande')))['total']
+    # licencias_alcohol
+    ws['E46'] = ProductosExtras.objects.filter(tipo='licencia_alcohol').count()
+    ws['E50'] = Pagos.objects.filter(pagado=True).aggregate(total=Count('solicitud__solicitud_lugar__extras', filter=Q(solicitud__solicitud_lugar__extras__tipo='licencia_alcohol')))['total']
+    ws['H46'] = ProductosExtras.objects.filter(tipo='licencia_alcohol').aggregate(total=Sum('precio'))['total']
+    ws['H50'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__extras__precio', filter=Q(solicitud__solicitud_lugar__extras__tipo='licencia_alcohol')))['total']
+
+    ws['Q29'] = Lugares.objects.all().aggregate(total=Sum('precio'))['total']
+    ws['Q32'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__precio'))['total']
+
+    ws['Q53'] = ProductosExtras.objects.all().aggregate(total=Sum('precio'))['total']
+    ws['Q56'] = Pagos.objects.filter(pagado=True).aggregate(total=Sum('solicitud__solicitud_lugar__extras__precio'))['total']
+
     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     wb.save(response)
     response['Content-Disposition'] = 'attachment; filename=REPORTE_SISCOMFERIA.xlsx'
