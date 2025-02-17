c01f4da4566a4f3a5c78d2a3e7b9d353ef51b6f3
DEDC <dedc_011596@hotmail.com>
2025-02-17 16:17:32 -0600

    Se agrega la función de restablecer contraseña

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,16 @@
 # Django
 from django.urls import path
 # users
-from apps.users.views import LoginUser, LogoutUser, CreateUser
+from apps.users.views import LoginUser, LogoutUser, CreateUser, CustomPasswordResetView, CustomPasswordResetConfirmView, CustomPasswordResetDoneView
 
 app_name = 'users'
 
 urlpatterns = [
     path('', LoginUser.as_view(), name='login'),
     path('logout', LogoutUser.as_view(), name='logout'),
-    path('registro', CreateUser.as_view(), name='create_user')
+    path('registro', CreateUser.as_view(), name='create_user'),
+    # passwordreset
+    path('password_reset', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_confirm/<uidb64>/<token>', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_done', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.views import PasswordResetDoneView
+from django.contrib.auth.views import PasswordResetConfirmView
+from django.contrib.auth.views import PasswordResetCompleteView
+from django.urls import reverse_lazy
 # users
 from apps.users.forms import UsersForm
 
@@ -31,3 +36,20 @@ class CreateUser(CreateView):
     def form_valid(self, form):
         form.instance.set_password(form.instance.password)    
         return super().form_valid(form)
+
+# PasswordReset
+class CustomPasswordResetView(PasswordResetView): #envía el token
+    template_name = 'usuarios/reset_password/password_reset_form.html'
+    email_template_name = 'usuarios/reset_password/password_reset_email.html'
+    html_email_template_name = 'usuarios/reset_password/password_reset_email.html'
+    subject_template_name = 'usuarios/reset_password/password_reset_subject.txt'
+    title = 'Restablezca su contraseña'
+    success_url = 'password_reset_done'
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView): #recibe el token
+    template_name = 'usuarios/reset_password/password_reset_confirm.html'
+    success_url = reverse_lazy('users:login')
+    post_reset_login = True
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'usuarios/reset_password/password_reset_done.html'
