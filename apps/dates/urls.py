54cecdf049889a09d2b31e2b11a05fe589b4b54b
DeBoleto <chrismendoza@deboleto.mx>
2025-02-20 10:45:16 -0600

    se libera el webhook para pruebas

--- apps/dates/urls.py
+++ apps/dates/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from apps.dates.views import get_available_dates, get_available_times, get_curp_service, PDFLineaCapturaView, \
-    TpayLineaCapturaView, TpayValidadoView
+    TpayLineaCapturaView, TpayValidadoView, WebHookTapyApiView
 # admin
 from apps.admin.views import render_place
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('places/pdfcpatura/<str:pk>', PDFLineaCapturaView.as_view(), name='pdfcpatura'),
     path('places/tpay/<str:pk>', TpayLineaCapturaView.as_view(), name='tpay-captura'),
     path('places/tpayval/<str:pk>', TpayValidadoView.as_view(), name='tpay-valid'),
+    path('webhook/tpay', WebHookTapyApiView.as_view(), name='tpay-webhook'),
 ]
