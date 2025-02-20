76c2b2a4569fbda49f3030279aa749ad2ef9acec
DEDC <dedc_011596@hotmail.com>
2025-02-19 21:50:27 -0600

    Se agrega la nave 2 sin uuids

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -27,7 +27,7 @@ from apps.users.forms import UserUpdateForm
 # dates
 from apps.dates.models import CitasAgendadas
 # utils
-from utils.naves import nave2
+from utils.naves import nave1, nave2, nave3
 from utils.permissions import AdminPermissions, AdminStaffPermissions
 from utils.date import get_date_constancy
 from utils.gafete import get_gafete
@@ -38,7 +38,7 @@ from utils.reports import get_report, get_requests_report
 from utils.email import send_html_mail
 from datetime import datetime
 
-places_dict = {'n_2': nave2,}
+places_dict = {'n_1': nave1, 'n_2': nave2, 'n_3': nave3,}
 
 dates = get_dates_from_range(settings.START_DATES, settings.END_DATES)
 hours = get_times_from_range(settings.START_HOURS, settings.END_HOURS, settings.PERIODS_TIME)
--- static/js/api/endpoints.js
+++ static/js/api/endpoints.js
@@ -1,4 +1,6 @@
 // Get all available dates and timess
 export const url_dates = '/api/get-available-dates'
 export const url_times = '/api/get-available-times'
+export const url_nave_1 = '/api/places/n_1/data'
 export const url_nave_2 = '/api/places/n_2/data'
+export const url_nave_3 = '/api/places/n_3/data'
