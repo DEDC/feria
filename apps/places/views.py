37e23115655cd6d9103237b57d56ef8b121de2a9
DeBoleto <chrismendoza@deboleto.mx>
2025-02-22 10:41:57 -0600

    modificaciones para desactivar en template la seleccion de lugares y apgos

--- apps/places/views.py
+++ apps/places/views.py
@@ -199,16 +199,7 @@ class Request(UserPermissions, DetailView):
         context['tpay_boardin'] = settings.TPAY_SESSION_ABORDAJE
         context['tpay_access'] = settings.TPAY_SESSION_ACCESS
         context['tpat_sistema'] = settings.TPAY_SISTEMA
-        if self.object.data_tpay:
-            tpay = self.object.data_tpay
-            if tpay:
-                try:
-                    context["pdf_url"] = tpay["data"]["urlFormatoPago"]["_text"]
-                    context["tpay_importe"] = tpay["data"]["importe"]["_text"]
-                    context["tpay_captura"] = tpay["data"]["lineaCaptura"]["_text"].split('|')[1]
-                    context["tpay_folio"] = tpay["data"]["folioControlEstado"]["_text"]
-                except Exception as e:
-                    print(e)
+        context["tpay_activo"] = False
         return context
 
 
