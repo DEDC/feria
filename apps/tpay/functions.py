edcc87361ea9c73a51df68f3eac039feaa820433
DeBoleto <chrismendoza@deboleto.mx>
2025-02-24 12:58:42 -0600

    modificaiones de procesos de validaciones

--- apps/tpay/functions.py
+++ apps/tpay/functions.py
@@ -1,7 +1,7 @@
 import json
 
 from apps.places.models import Lugares, HistorialTapy
-from apps.tpay.tools import consulta_linea_captura, generarToken, status_linea_captura
+from apps.tpay.tools import consulta_linea_captura, generarToken, status_linea_captura, validar_linea_captura
 from feria import settings
 
 
@@ -35,25 +35,49 @@ def status_validar_pago(id, historico=False):
         data_tpay = hist.data_tpay
         tpay_folio = hist.tpay_folio
 
-    # Obtener el PDF usando requests
-    user_data = json.dumps({
-        "user": lugar.solicitud.nombre,
-        "email": lugar.solicitud.usuario.email
+    data = json.dumps({
+        "orderId": "2025-{}".format(tpay_folio),
+        "sistemaId": 21,
+        "proyecto": settings.TPAY_PROJECT,
+        "monto": lugar.data_tpay["importe"]
     }, separators=(",", ":")
     )
-    token, error = generarToken(user_data)
+    status = status_linea_captura("", data)
 
-    if not error:
-        data = json.dumps({
-            "orderId": "2025-{}".format(tpay_folio),
-            "sistemaId": 21,
-            "proyecto": settings.TPAY_PROJECT,
-            "monto": lugar.data_tpay["importe"]
+    if status["codigoEstatus"] == 0:
+        # Obtener el PDF usando requests
+        user_data = json.dumps({
+            "user": lugar.solicitud.nombre,
+            "email": lugar.solicitud.usuario.email
         }, separators=(",", ":")
         )
-        status = status_linea_captura(token, data)
-        lugar.tpay_status = status
-        return status
+        token, error = generarToken(user_data)
+
+        if not error:
+            if not error:
+                data = json.dumps({
+                    "AuthS701": data["data"]["pnum_autorizacion"],
+                    "referenceKey": data["data"]["pordenp_ref"],
+                    "AccessUser": 'BBV',  # data["data"]["resultadoTrans"]["pcve_instrumento_pago"],
+                    "EstablishNum": "7681",
+                    "BranchSource": "7681",
+                }, separators=(",", ":")
+                )
+
+                validacion = validar_linea_captura(token, data)
+                print(validacion)
+                if "res" in validacion:
+                    lugar.tpay_service = True
+    lugar.tpay_status = data_tpay
+    lugar.data_tpay = data_tpay
+    lugar.tpay_val = status
+    lugar.tpay_pagado = True
+    lugar.tpay_web = True
+    lugar.tpay_service = True
+    lugar.tpay_socket = True
+    lugar.tpay_folio = tpay_folio
+    lugar.save()
+    return status
 
 def updateFolioHistorial():
     historial = HistorialTapy.objects.all()
