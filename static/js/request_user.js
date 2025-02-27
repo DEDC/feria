0a93e6dc2a20345dbe108dc0e44a41c97e8291b1
DeBoleto <chrismendoza@deboleto.mx>
2025-02-26 21:45:05 -0600

    modificaciones de validaciones para pagos

--- static/js/request_user.js
+++ static/js/request_user.js
@@ -71,8 +71,8 @@ document.addEventListener('DOMContentLoaded', () => {
             }).catch((error) => {
                 console.log(error);
                 Swal.fire({
-                  title: "Plataforma de Pago",
-                  text: "Por el momento se encuentra deshabilitada la plataforma de pago, favor de intentar el pago en un momento.",
+                  title: "Estatus del pago",
+                  text: "El pago se encuentra en procesado de validacion, espere un momento.",
                   icon: "warning"
                 });
             });
