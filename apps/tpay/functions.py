7a0803b8daa75fc4079c5144f4e7de01d27132f6
DeBoleto <chrismendoza@deboleto.mx>
2025-03-02 22:04:45 -0600

    MNdama

--- apps/tpay/functions.py
+++ apps/tpay/functions.py
@@ -137,7 +137,7 @@ def process_validated_pay():
             if status["respuesta"] == False:
                 # if status["codigoEstatus"] != 0:
                     # Obtener el PDF usando requests
-                    # HistorialTapy.objects.filter(lugar=l).delete()
+                HistorialTapy.objects.filter(lugar=l).delete()
                 l.delete()
                 print(status)
                 process += 1
