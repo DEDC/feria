221836546dc3345d8999b6d7dd8b9183fa527c29
DEDC <dedc_011596@hotmail.com>
2025-03-20 17:07:29 -0600

    Se agrega la página de aviso de privacidad

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,5 +1,6 @@
 # Django
 from django.urls import path
+from django.views.generic import TemplateView
 # users
 from apps.users.views import LoginUser, LogoutUser, CreateUser, CustomPasswordResetView, CustomPasswordResetConfirmView, CustomPasswordResetDoneView
 
@@ -9,6 +10,7 @@ urlpatterns = [
     path('', LoginUser.as_view(), name='login'),
     path('logout', LogoutUser.as_view(), name='logout'),
     path('registro', CreateUser.as_view(), name='create_user'),
+    path('aviso_privacidad', TemplateView.as_view(template_name='usuarios/privacidad.html'), name='aviso_privacidad'),
     # passwordreset
     path('password_reset', CustomPasswordResetView.as_view(), name='password_reset'),
     path('password_reset_confirm/<uidb64>/<token>', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
