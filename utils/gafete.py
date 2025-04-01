7e53d7b7d8a080a857a11167b1f0408154f14ab5
DEDC <dedc_011596@hotmail.com>
2025-04-01 03:33:04 -0600

    Se agrega el gafete de ambulante

--- utils/gafete.py
+++ utils/gafete.py
@@ -1,36 +1,46 @@
 # Python
 import io
-import textwrap
 # Django
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
 # Reportlab and PyPDF2
 from PyPDF2 import PdfWriter, PdfReader
 from reportlab.graphics.barcode import qr
 from reportlab.graphics.shapes import Drawing
 from reportlab.graphics import renderPDF
 from reportlab.pdfgen.canvas import Canvas
-from reportlab.lib.pagesizes import letter
+# Python
+import io
+from textwrap import wrap
+# Django
+from django.http import HttpResponse
 
 def get_gafete(place):
     output = PdfWriter()
-    inputw = PdfReader(open('static/docs/gafete.pdf', 'rb'))
+    if place.zona == 'amb':
+        url = 'static/docs/gafete_ambulante.pdf'
+    else:
+        raise ValidationError('No se generó gafete para esa Zona. Intente de nuevo más tarde.', code = 'invalid_gafete')
+    
+    inputw = PdfReader(open(url, 'rb'))
     buffer = io.BytesIO()
     pdf = Canvas(buffer)
-    width, height = letter
-    # username
-    pdf.setFont("Helvetica", 8)
-    pdf.drawString(107, 162, place.get_zona_display())
-    pdf.drawString(163, 162, place.nombre)
-    pdf.drawString(82, 147, textwrap.wrap(place.solicitud.nombre.upper(), 24)[0])
+    pdf.setFont("Helvetica-Bold", 33)
+    text = place.solicitud.nombre.upper()
+    for i, s in enumerate(wrap(text, 23)):
+        y_step  = 35 * i
+        y_start = 480 - y_step
+        pdf.drawCentredString(305, y_start, s)
+        pdf.drawCentredString
     # QR
     qr_text = str(place.folio)
     qr_code = qr.QrCodeWidget(qr_text)
     bounds = qr_code.getBounds()
     width = bounds[2] - bounds[0]
     height = bounds[3] - bounds[1]
-    d = Drawing(100, 100, transform=[130./width, 0, 0, 130./height, 0, 0])
+    d = Drawing(100, 100, transform=[160./width, 0, 0, 160./height, 0, 0])
     d.add(qr_code)
-    renderPDF.draw(d, pdf, 71, 20)
+    renderPDF.draw(d, pdf, 229, 120)
     # Creación del pdf final
     pdf.save()
     watermark = PdfReader(buffer)
